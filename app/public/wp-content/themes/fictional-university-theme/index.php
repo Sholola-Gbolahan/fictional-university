8cf0572d9d104f2c2af82fe14ce5a2d5e6d6a3e8
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-25 22:24:08 +0100

    Post, pages and single post setup done

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,12 +1,22 @@
-<?php 
-// 1. Creating an array of names
-$name = array('Brad','Tosin', 'Jane','Adebato','Gideon');
-$count = 0;
+<?php  
+// 1. Accessing wordpress created post with predefined function
+// 2. The while loop condition says: keep looping as long as there's post
+while(have_posts()){
+    //3. the_post() is a wordpress function that does several important things
+    // It keep track of the post we're currently working with 
+    // the_post() tells wordpress to get all relevant information each time while loops run 
+    the_post();?>
 
-// Looping through the array using while loop nad dertermin the array length with count() function
-while( $count < count($name)){
-    echo "<li>Hi, my name is $name[$count]</li>";
-    $count++;
+    <!-- 4. Dropping out of PHP to HTML while still in WHILE LOOP. -->
+    <!-- 5. the_title() : give the title of a post -->
+     <!-- 7. the_permalink(): It's the function for a sigle page in wordpress -->
+    <h2> <a href="<?php the_permalink(); ?>"><?php  the_title();?> </a></h2>
+    <!-- 6. the_content(): give the post content -->
+    <?php the_content(); ?>
+    <hr>
+
+    <?php  
+
 }
 
 ?>
