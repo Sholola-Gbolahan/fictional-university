72a683ae2d3c79edb85f1b8516b9fd277ff5b578
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-25 16:05:13 +0100

    Setting up an array and a while loop

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,12 @@
-An Amazing custom theme
+<?php 
+// 1. Creating an array of names
+$name = array('Brad','Tosin', 'Jane','Adebato','Gideon');
+$count = 0;
+
+// Looping through the array using while loop nad dertermin the array length with count() function
+while( $count < count($name)){
+    echo "<li>Hi, my name is $name[$count]</li>";
+    $count++;
+}
+
+?>
