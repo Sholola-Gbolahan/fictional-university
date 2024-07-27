4df3e919080559ba657bd67f08171f5525df62c5
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-27 05:10:56 +0100

    How to add scripts, external links to header, Code to page, navigate to import from other folders

--- app/public/wp-content/themes/fictional-university-theme/footer copy.php
+++ app/public/wp-content/themes/fictional-university-theme/footer copy.php
@@ -58,7 +58,7 @@
         </div>
       </div>
     </footer>
+
 <?php wp_footer(); ?>
 </body>
-
-</html> 
+</html>
--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,15 +1,20 @@
 <?php  
 
 function university_files() { 
-    // this is used to load our css. It's expecting 2 arg.
-    //first: the nickname of the styling . second: the location of the file
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());   
+
+    // Adding a script file required the use of wp_enqueue_script() 
+    // Argument accepted by this function 
+    //  1. file Name, 2. script location 3. dependency Array, 4. script version number, 5. A selection of  true of false to allow WP load the script before the body tag
+    wp_enqueue_script('main-university-js', get_theme_file_uri('./build/index.js'), array('jquery'), '1.0', true );
+    // 3. Loading custom google font for our page
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    // 2. Loading of an external link font-awesome styling in wordpress
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'); 
+    // 1. get_theme_file_uri() : Direct WP to loaction of an exiting file to load from
+    //  instead of get_file_uri() that only get style from the general WP style.css
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));   
+
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css')); 
 }
 
-//  add_action(): it's a function used to tell wordpress what to do.
-//  It's accept two argument where the first is where we tell function type of instruction we're giving
-// Second is where we give WP the name of function we want to run
-
-//  wp_enqueue_scripts : is an instruction telling wordpress we want to load css or javascript files
-// excluding parenthenses at the function university_files mean we don't want to run it now leaving WP to do at it's time
 add_action('wp_enqueue_scripts','university_files');
